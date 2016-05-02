e25a93e70dc7ec1765b9c0fbc986f74ae1185bb9
James Montgomerie <jamie@montgomerie.net>
2016-05-01 23:10:24 -0700

    Working LCD display of things!

--- src/main.cpp
+++ src/main.cpp
@@ -2,63 +2,60 @@
 * Techniques from http://www.dataweek.co.za/article.aspx?pklArticleId=2382&pklCategoryId=31
 */
 
-#include "Arduino.h"
-#include <avr/pgmspace.h> 
+#include <Arduino.h> 
 
 #ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define cbi(sfr, bit) (sfr &= ~_BV(bit))
 #endif
 #ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#define sbi(sfr, bit) (sfr |= _BV(bit))
 #endif 
 
-enum TriState {
-	off = false,
-	on = true,
-	nc,
+struct PortDescriptor {
+	typeof(PORTB) * const port;
+	typeof(DDRB) * const ddr; 
+	const byte offset;
 };
 
-static const TriState commons[8][4] = { 
-	{ on,  nc,  nc,  nc  },
-	{ nc,  on,  nc,  nc  },
-	{ nc,  nc,  on,  nc  },
-	{ nc,  nc,  nc,  on  },
-	{ off, nc,  nc,  nc  },
-	{ nc,  off, nc,  nc  },
-	{ nc,  nc,  off, nc  },
-	{ nc,  nc,  nc,  off },
-};
-
-static const struct { const typeof(PORTB) *port; const typeof(DDRB) *ddr; const byte offset;} commonPorts[4] = {
+static const PortDescriptor commonPorts[4] = {
 	{ &PORTB, &DDRB, 0 },
 	{ &PORTD, &DDRD, 7 },
 	{ &PORTD, &DDRD, 6 },
 	{ &PORTD, &DDRD, 5 },
 };
 
-static const struct { const typeof(PORTB) *port; const byte offset; } segmentPorts[12] = {
-	{ &PORTB, 1 },
-	{ &PORTB, 2 },
-	{ &PORTB, 3 },
-	{ &PORTB, 4 },
-	{ &PORTC, 0 },
-	{ &PORTC, 1 },
-	{ &PORTC, 2 },
-	{ &PORTC, 3 },
-	{ &PORTC, 4 },
-	{ &PORTC, 5 },
-	{ &PORTD, 4 },
-	{ &PORTD, 3 },
+static const PortDescriptor segmentPorts[12] = {
+	{ &PORTB, &DDRB, 1 },
+	{ &PORTB, &DDRB, 2 },
+	{ &PORTB, &DDRB, 3 },
+	{ &PORTB, &DDRB, 4 },
+	{ &PORTC, &DDRC, 0 },
+	{ &PORTC, &DDRC, 1 },
+	{ &PORTC, &DDRC, 2 },
+	{ &PORTC, &DDRC, 3 },
+	{ &PORTC, &DDRC, 4 },
+	{ &PORTC, &DDRC, 5 },
+	{ &PORTD, &DDRD, 4 },
+	{ &PORTD, &DDRD, 3 },
 };
 
 static const int LCD_AC_FREQ = 31;
 
 void setup()
 {	
-	noInterrupts(); 
+	for(auto port : commonPorts) {
+		sbi(*port.ddr, port.offset);
+		sbi(*port.port, port.offset);
+	}
+	for(auto port : segmentPorts) {
+		sbi(*port.ddr, port.offset);
+		sbi(*port.port, port.offset);
+	}
 
-	static const int timer_reset_count = ((F_CPU / 1024) / (LCD_AC_FREQ * 8));
+	static const int timer_reset_count = ((F_CPU / 1024) / ((int)LCD_AC_FREQ * (int)8));
 	static_assert(timer_reset_count <= 255, "timer_reset_count is too large!");
+
+	noInterrupts(); 
 	
 	#ifdef __AVR_ATmega8__
 	{
@@ -96,55 +93,61 @@ ISR(TIMER2_COMP_vect)
 #else
 ISR(TIMER2_COMPA_vect)        // interrupt service routine 
 #endif
-{	
-	
-	static byte phase = 0;
-	phase = (phase + 1)	% 8;
+{		
+	static byte counter = 0;
+	static byte segmentTestPhase = 0;
+	static byte segmentItemTestPhase = 0;
+
+	const byte phase = (counter % 8);
+	const byte common = (phase % 4);
+	const bool commonUpPhase = (phase < 4);
 	
 	{
 		byte i = 0;
-		for(auto state : commons[phase]) {
-			auto port = commonPorts[i];
-			switch(state) {
-				case on:
-					sbi(*port.ddr, port.offset);
+		for(auto port : commonPorts) {
+			if(i == common) {
+				sbi(*port.ddr, port.offset);
+				if(commonUpPhase) {
 					sbi(*port.port, port.offset);
-					break;
-				case off:	
-					sbi(*port.ddr, port.offset);
+				} else {
 					cbi(*port.port, port.offset);
-					break;
-				case nc:
-					cbi(*port.ddr, port.offset);
-					cbi(*port.port, port.offset);
-					break;
+				}
+			} else {
+				cbi(*port.ddr, port.offset);	
+				sbi(*port.port, port.offset);
 			}
 			++i;
 		}
 	}
 	
-	{	
-		static byte segmentTestPhase = 0;
-		if(phase == 0) {
-			segmentTestPhase = (segmentTestPhase + 1) % 12;
-		}
-
+	{
 		byte i = 0;
 		for(auto port : segmentPorts) {
-			if(i == segmentTestPhase) {
-				if(phase < 4) {
+			if(i == segmentTestPhase &&
+			   common == segmentItemTestPhase) {
+				if(commonUpPhase) {
 					cbi(*port.port, port.offset);
 				} else {
 					sbi(*port.port, port.offset);
 				}
 			} else {
-				if(phase < 4) {
+				if(commonUpPhase) {
 					sbi(*port.port, port.offset);
 				} else {
 					cbi(*port.port, port.offset);
 				}
 			}
 			++i;
+		}
+	}
+	
+	if(++counter == 64) {
+		counter = 0;
+		if(++segmentTestPhase == 12) {
+			segmentTestPhase = 0;
+			if(++segmentItemTestPhase == 4) {
+				segmentItemTestPhase = 0;
+			}
 		}
 	}
 }
