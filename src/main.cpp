25db44ce2c30301bfdbe358cd57402a01d11c8c8
James Montgomerie <jamie@montgomerie.net>
2019-08-15 21:43:03 -0700

    Working test code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 /**
-* Techniques from http://www.dataweek.co.za/article.aspx?pklArticleId=2382&pklCategoryId=31
+* Techniques from DataWeek "Bare LCD display drive in embedded applications"
+* http://www.dataweek.co.za/article.aspx?pklArticleId=2382&pklCategoryId=31
 */
 
 #include <Arduino.h> 
+#include <avr/sleep.h> 
+
 
 #ifndef cbi
 #define cbi(sfr, bit) (sfr &= ~_BV(bit))
@@ -10,81 +13,102 @@
 #ifndef sbi
 #define sbi(sfr, bit) (sfr |= _BV(bit))
 #endif 
+#ifndef tbi
+#define tbi(sfr, bit) ((sfr&_BV(bit))?cbi(sfr, bit):sbi(sfr, bit))
+#endif 
+
 
 struct PortDescriptor {
-	typeof(PORTB) * const port;
-	typeof(DDRB) * const ddr; 
-	const byte offset;
+    const byte portNumber;
+    const byte offset;
+};
+
+static const typeof(&PORTB) Ports[] = {
+    &PORTB,
+    &PORTC,
+    &PORTD,
+};
+
+static const typeof(&DDRB) DDRs[] = {
+    &DDRB,
+    &DDRC,
+    &DDRD,
 };
 
 static const PortDescriptor commonPorts[4] = {
-	{ &PORTB, &DDRB, 0 },
-	{ &PORTD, &DDRD, 7 },
-	{ &PORTD, &DDRD, 6 },
-	{ &PORTD, &DDRD, 5 },
+    { 2, 5 },
+    { 2, 4 },
+    { 2, 3 },
+    { 2, 2 },
 };
 
 static const PortDescriptor segmentPorts[12] = {
-	{ &PORTB, &DDRB, 1 },
-	{ &PORTB, &DDRB, 2 },
-	{ &PORTB, &DDRB, 3 },
-	{ &PORTB, &DDRB, 4 },
-	{ &PORTC, &DDRC, 0 },
-	{ &PORTC, &DDRC, 1 },
-	{ &PORTC, &DDRC, 2 },
-	{ &PORTC, &DDRC, 3 },
-	{ &PORTC, &DDRC, 4 },
-	{ &PORTC, &DDRC, 5 },
-	{ &PORTD, &DDRD, 4 },
-	{ &PORTD, &DDRD, 3 },
+    { 2, 6 },
+    { 2, 7 },
+    { 0, 0 },
+    { 0, 1 },
+    { 0, 2 },
+    { 0, 3 },
+    { 0, 4 },
+    { 1, 0 },
+    { 1, 1 },
+    { 1, 2 },
+    { 1, 3 },
+    { 1, 4 },
 };
 
-static const int LCD_AC_FREQ = 31;
+static const int LCD_AC_FREQ = 32;
 
 void setup()
 {	
-	for(auto port : commonPorts) {
-		sbi(*port.ddr, port.offset);
-		sbi(*port.port, port.offset);
-	}
-	for(auto port : segmentPorts) {
-		sbi(*port.ddr, port.offset);
-		sbi(*port.port, port.offset);
-	}
-
-	static const int timer_reset_count = ((F_CPU / 1024) / ((int)LCD_AC_FREQ * (int)8));
-	static_assert(timer_reset_count <= 255, "timer_reset_count is too large!");
-
-	noInterrupts(); 
-	
-	#ifdef __AVR_ATmega8__
-	{
-		TCCR2 = 0;
-		TCCR2 |= (1 << WGM21);              	           // CTC ("Clear Timer/Counter") Mode
-		TCCR2 |= (1 << CS22) | (1 << CS21) | (1 << CS20);  // A 1024 prescaler  
-		
-		TIMSK |= (1 << OCIE2);                            // Output Compare Interupt Enable
-		
-		// Set up the counter output compare to get the desired frequency.
-		// Check for overflow, just in case...
-		OCR2 = timer_reset_count;	
-		TCNT2 = 0;
-	}
-	#else
-	{
-		TCCR2A = (1 << WGM21);              	           // CTC ("Clear Timer/Counter") Mode
-		TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20);  // A 1024 prescaler  
-	
-		TIMSK2 = (1 << OCIE2A);                            // Output Compare Interupt Enable
-		
-		// Set up the counter output compare to get the desired frequency.
-		// Check for overflow, just in case...
-		OCR2A = timer_reset_count;	
-		TCNT2 = 0;
-	}
-	#endif
-	
-	interrupts();
+    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+    
+    for(auto port : commonPorts) {
+        sbi(*DDRs[port.portNumber], port.offset);
+        cbi(*Ports[port.portNumber], port.offset);
+    }
+    for(auto port : segmentPorts) {
+        sbi(*DDRs[port.portNumber], port.offset);
+        cbi(*Ports[port.portNumber], port.offset);
+    }
+    
+    sbi(DDRB, 5);
+    cbi(PORTB, 5);
+    
+    static const int timer_reset_count = ((F_CPU / 1024) / ((int)LCD_AC_FREQ * (int)8));
+    static_assert(timer_reset_count <= 255, "timer_reset_count is too large!");
+    
+    
+    noInterrupts(); 
+    
+    #ifdef __AVR_ATmega8__
+    {
+        TCCR2 = 0;
+        TCCR2 |= (1 << WGM21);              	           // CTC ("Clear Timer/Counter") Mode
+        TCCR2 |= (1 << CS22) | (1 << CS21) | (1 << CS20);  // A 1024 prescaler  
+        
+        TIMSK |= (1 << OCIE2);                            // Output Compare Interupt Enable
+        
+        // Set up the counter output compare to get the desired frequency.
+        // Check for overflow, just in case...
+        OCR2 = timer_reset_count;	
+        TCNT2 = 0;
+    }
+    #else
+    {
+        TCCR2A = (1 << WGM21);              	            // CTC ("Clear Timer/Counter") Mode
+        TCCR2B = (1 << CS22) | (1 << CS21) | (1 << CS20);  // A 1024 prescaler  
+        
+        TIMSK2 = (1 << OCIE2A);                            // Output Compare Interupt Enable
+        
+        // Set up the counter output compare to get the desired frequency.
+        // Check for overflow, just in case...
+        OCR2A = timer_reset_count;	
+        TCNT2 = 0;
+    }
+    #endif
+    
+    interrupts();
 }
 
 // interrupt service routine 
@@ -93,65 +117,93 @@ ISR(TIMER2_COMP_vect)
 #else
 ISR(TIMER2_COMPA_vect)        // interrupt service routine 
 #endif
-{		
-	static byte counter = 0;
-	static byte segmentTestPhase = 0;
-	static byte segmentItemTestPhase = 0;
+{
+    static byte counter = 0;
+    static byte segmentTestPhase = 0;
+    static byte commonTestPhase = 0;
+    
+#if 1
+    // From DataWeek "Bare LCD display drive in embedded applications"
+    const byte phase = (counter % 8);
+    const byte common = (phase % 4);
+    const bool commonUpPhase = (phase < 4);
+#else
+    // From AVR340 Application Note. 
+    const byte phase = (counter % 8);
+    const byte common = (phase / 2);
+    const bool commonUpPhase = (phase % 2);
+#endif
 
-	const byte phase = (counter % 8);
-	const byte common = (phase % 4);
-	const bool commonUpPhase = (phase < 4);
-	
-	{
-		byte i = 0;
-		for(auto port : commonPorts) {
-			if(i == common) {
-				sbi(*port.ddr, port.offset);
-				if(commonUpPhase) {
-					sbi(*port.port, port.offset);
-				} else {
-					cbi(*port.port, port.offset);
-				}
-			} else {
-				cbi(*port.ddr, port.offset);	
-				sbi(*port.port, port.offset);
-			}
-			++i;
-		}
-	}
-	
-	{
-		byte i = 0;
-		for(auto port : segmentPorts) {
-			if(i == segmentTestPhase &&
-			   common == segmentItemTestPhase) {
-				if(commonUpPhase) {
-					cbi(*port.port, port.offset);
-				} else {
-					sbi(*port.port, port.offset);
-				}
-			} else {
-				if(commonUpPhase) {
-					sbi(*port.port, port.offset);
-				} else {
-					cbi(*port.port, port.offset);
-				}
-			}
-			++i;
-		}
-	}
-	
-	if(++counter == 64) {
-		counter = 0;
-		if(++segmentTestPhase == 12) {
-			segmentTestPhase = 0;
-			if(++segmentItemTestPhase == 4) {
-				segmentItemTestPhase = 0;
-			}
-		}
-	}
+    noInterrupts(); 
+
+    byte buildPorts[] = { *Ports[0], *Ports[1], *Ports[2] };
+    byte buildDDRs[] = { *DDRs[0], *DDRs[1], *DDRs[2] };
+        
+    {
+        byte i = 0;
+        for(auto port : commonPorts) {
+            if(i == common) {
+                sbi(buildDDRs[port.portNumber], port.offset);
+                if(commonUpPhase) {
+                    sbi(buildPorts[port.portNumber], port.offset);
+                } else {
+                    cbi(buildPorts[port.portNumber], port.offset);
+                }
+            } else {
+                cbi(buildDDRs[port.portNumber], port.offset);	
+                cbi(buildPorts[port.portNumber], port.offset);
+            }
+            ++i;
+        }
+    }
+
+    {
+        byte i = 0;
+        for(auto port : segmentPorts) {
+            if(i == segmentTestPhase && common == commonTestPhase) {
+                if(commonUpPhase) {
+                    cbi(buildPorts[port.portNumber], port.offset);
+                } else {
+                    sbi(buildPorts[port.portNumber], port.offset);
+                }
+            } else {
+                if(commonUpPhase) {
+                    sbi(buildPorts[port.portNumber], port.offset);
+                } else {
+                    cbi(buildPorts[port.portNumber], port.offset);
+                }
+            }
+            ++i;
+        }
+    }
+        
+    for(int i = 0; i < 3; ++i) {
+        if(*DDRs[i] != buildDDRs[i]) {
+            *DDRs[i] = buildDDRs[i];
+        }
+        if(*Ports[i] != buildPorts[i]) {
+            *Ports[i] = buildPorts[i];
+        }
+    }
+    
+    interrupts();
+   
+    ++counter;
+    
+    if((counter % (LCD_AC_FREQ * 2)) == 0) {
+        counter = 0;
+        if(++segmentTestPhase == 12) {
+            segmentTestPhase = 0;
+            if(++commonTestPhase == 4) {
+                commonTestPhase = 0;
+            }
+        }
+    }
 }
 
 void loop()
 {
+    
+	    
 }
+
