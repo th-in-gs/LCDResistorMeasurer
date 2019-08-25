3d0e0f55eb1d9e498296b791f5d677daaf470cdb
James Montgomerie <jamie@montgomerie.net>
2019-08-24 23:32:07 -0700

    LCD routines done(?) = counting up.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include <Arduino.h> 
 #include <avr/sleep.h> 
 
-
 #ifndef cbi
 #define cbi(sfr, bit) (sfr &= ~_BV(bit))
 #endif
@@ -57,50 +56,205 @@ static const PortDescriptor segmentPorts[12] = {
     { 0, 5 }, // LCD Pin 14
 };
 
-static const uint8_t SevenSegmentCodes[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
+// Want to map symbolic name to array of ports and segments.
+// Also want to map digits and positions to array of ports and segments.
 
-static const uint16_t LCDPinToBitPosition[19] = {
-    0,       // N/A
-    _BV(11), // Pin  1
-    _BV(12), // Pin  2
-    _BV( 0), // Pin  3
-    _BV( 1), // Pin  4
-    _BV( 2), // Pin  5
-    _BV( 3), // Pin  6
-    _BV( 4), // Pin  7
-    _BV( 5), // Pin  8
-    _BV( 6), // Pin  9
-    _BV( 7), // Pin 10
-    _BV( 8), // Pin 11
-    _BV( 9), // Pin 12
-    _BV(10), // Pin 13
-    _BV(11), // Pin 14
-    0,       // Pin 15
-    0,       // Pin 16
-    _BV(13), // Pin 17
-    _BV(14), // Pin 18
+// First, define symbolic names.
+class LEDSymbol {
+public:
+    enum Name {
+        DottedUpsidedownT,
+        ChartGoingNegative,
+        BLANK,
+        DownArrowOnLeft,
+        SevenSeg1E,
+        MinusLarge,
+        SevenSeg1A,
+        SevenSeg1F,
+        SevenSeg1C,
+        SevenSeg1D,
+        SevenSeg1B,
+        SevenSeg1G,
+        SevenSeg2E,
+        Colon,
+        SevenSeg2A,
+        SevenSeg2F,
+        SevenSeg2C,
+        SevenSeg2D,
+        SevenSeg2B,
+        SevenSeg2G,
+        SevenSeg3E,
+        DecimalPoint,
+        SevenSeg3A,
+        SevenSeg3F,
+        SevenSeg3C,
+        SevenSeg3D,
+        SevenSeg3B,
+        SevenSeg3G,
+        FireCrossed,
+        CautionTriangle,
+        MinusLeft,
+        DownArrowOnRight,
+        Percent,
+        Celcius,
+        MilliAmp,
+        Bar,
+        UpArrowOnRight,
+        Plus,
+        Blower,
+        Fire,
+        Tap,
+        Two,
+        Radiator,
+        TriangleInCircle,
+        Sun,
+        Target,
+        One,
+        UpArrowOnLeft,
+    };
+
+    LEDSymbol() = default; 
+    constexpr LEDSymbol(Name name) : _name(name) {}
+
+    constexpr uint8_t CommonPin() const { return (uint8_t)(_name % 4); };
+    constexpr uint16_t SegmentPin() const { return (uint8_t)(_name / 4); };
+
+private:
+    Name _name;
 };
 
-
-struct CommonAndSegment {
-    uint8_t commonPin;
-    uint8_t segmentPin;
-};
-
-static const CommonAndSegment CommonAndSegmentNULL = { 0, 0 };
-
-static const 
-
-/*
-static uint16_t numberPattern(uint8_t digit, uint8_t position) {
-    for (uint8_t i = 0, bits = SevenSegmentCodes[digit]; bits > 0; bits >>= 1, ++i) {
-        if(bits & 0b1) {
-
-        }
-        ++i;
+static const LEDSymbol DigitSegmentSymbols[3][7] = {
+    {
+        LEDSymbol::SevenSeg3A,
+        LEDSymbol::SevenSeg3B,
+        LEDSymbol::SevenSeg3C,
+        LEDSymbol::SevenSeg3D,
+        LEDSymbol::SevenSeg3E,
+        LEDSymbol::SevenSeg3F,
+        LEDSymbol::SevenSeg3G,
+    },
+    {
+        LEDSymbol::SevenSeg2A,
+        LEDSymbol::SevenSeg2B,
+        LEDSymbol::SevenSeg2C,
+        LEDSymbol::SevenSeg2D,
+        LEDSymbol::SevenSeg2E,
+        LEDSymbol::SevenSeg2F,
+        LEDSymbol::SevenSeg2G,
+    },
+    {
+        LEDSymbol::SevenSeg1A,
+        LEDSymbol::SevenSeg1B,
+        LEDSymbol::SevenSeg1C,
+        LEDSymbol::SevenSeg1D,
+        LEDSymbol::SevenSeg1E,
+        LEDSymbol::SevenSeg1F,
+        LEDSymbol::SevenSeg1G,
     }
-}
-*/
+};
+
+static const uint16_t DigitSegmentPortMaskForCommon[4] {
+      (DigitSegmentSymbols[0][0].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[0][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][1].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[0][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][2].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[0][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][3].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[0][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][4].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[0][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][5].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[0][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][6].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[0][6].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][0].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[1][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][1].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[1][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][2].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[1][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][3].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[1][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][4].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[1][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][5].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[1][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][6].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[1][6].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][0].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[2][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][1].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[2][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][2].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[2][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][3].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[2][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][4].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[2][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][5].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[2][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][6].CommonPin() == 0 ? (uint16_t)_BV(DigitSegmentSymbols[2][6].SegmentPin()) : 0),
+    
+      (DigitSegmentSymbols[0][0].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[0][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][1].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[0][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][2].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[0][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][3].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[0][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][4].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[0][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][5].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[0][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][6].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[0][6].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][0].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[1][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][1].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[1][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][2].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[1][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][3].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[1][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][4].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[1][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][5].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[1][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][6].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[1][6].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][0].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[2][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][1].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[2][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][2].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[2][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][3].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[2][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][4].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[2][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][5].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[2][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][6].CommonPin() == 1 ? (uint16_t)_BV(DigitSegmentSymbols[2][6].SegmentPin()) : 0),
+
+      (DigitSegmentSymbols[0][0].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[0][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][1].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[0][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][2].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[0][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][3].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[0][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][4].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[0][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][5].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[0][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][6].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[0][6].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][0].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[1][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][1].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[1][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][2].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[1][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][3].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[1][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][4].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[1][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][5].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[1][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][6].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[1][6].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][0].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[2][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][1].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[2][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][2].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[2][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][3].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[2][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][4].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[2][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][5].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[2][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][6].CommonPin() == 2 ? (uint16_t)_BV(DigitSegmentSymbols[2][6].SegmentPin()) : 0),
+
+      (DigitSegmentSymbols[0][0].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[0][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][1].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[0][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][2].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[0][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][3].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[0][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][4].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[0][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][5].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[0][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[0][6].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[0][6].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][0].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[1][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][1].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[1][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][2].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[1][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][3].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[1][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][4].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[1][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][5].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[1][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[1][6].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[1][6].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][0].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[2][0].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][1].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[2][1].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][2].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[2][2].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][3].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[2][3].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][4].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[2][4].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][5].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[2][5].SegmentPin()) : 0)
+    | (DigitSegmentSymbols[2][6].CommonPin() == 3 ? (uint16_t)_BV(DigitSegmentSymbols[2][6].SegmentPin()) : 0)
+};
+
+static const uint8_t SevenSegmentCodes[10] = { 
+    0b0111111, 
+    0b0000110, 
+    0b1011011, 
+    0b1001111, 
+    0b1100110, 
+    0b1101101, 
+    0b1111101, 
+    0b0000111, 
+    0b1111111,
+    0b1101111 
+};
 
 static const uint8_t LCD_AC_FREQ = 32;
 
@@ -117,12 +271,8 @@ void setup()
         cbi(*Ports[port.portNumber], port.offset);
     }
     
-    sbi(DDRB, 5);
-    cbi(PORTB, 5);
-    
     static const uint16_t timer_reset_count = ((F_CPU / 1024) / ((uint16_t)LCD_AC_FREQ * (uint16_t)8));
     static_assert(timer_reset_count <= 255, "timer_reset_count is too large!");
-    
     
     noInterrupts(); 
     
@@ -156,6 +306,8 @@ void setup()
     interrupts();
 }
 
+static uint16_t displaySegmentStateByCommon[4] = {0};
+
 // uint16_terrupt service routine 
 #ifdef __AVR_ATmega8__
 ISR(TIMER2_COMP_vect)
@@ -163,27 +315,28 @@ ISR(TIMER2_COMP_vect)
 ISR(TIMER2_COMPA_vect)        // uint16_terrupt service routine 
 #endif
 {
-    static uint8_t counter = 0;
-    static uint8_t segmentTestPhase = 0;
-    static uint8_t commonTestPhase = 0;
+    static uint8_t phase = 0;
     
 #if 1
     // From DataWeek "Bare LCD display drive in embedded applications"
-    const uint8_t phase = (counter % 8);
     const uint8_t common = (phase % 4);
     const bool commonUpPhase = (phase < 4);
 #else
     // From AVR340 Application Note. 
-    const uint8_t phase = (counter % 8);
     const uint8_t common = (phase / 2);
     const bool commonUpPhase = (phase % 2);
 #endif
+
+    // We'll copy this from global storage once per cycle - see below.
+    static uint16_t segmentPinStateByCommon[4] = {0};
+
+    uint16_t segmentPinsToLight = segmentPinStateByCommon[common];
 
     noInterrupts(); 
 
     uint8_t buildPorts[] = { *Ports[0], *Ports[1], *Ports[2] };
     uint8_t buildDDRs[] = { *DDRs[0], *DDRs[1], *DDRs[2] };
-        
+
     {
         uint8_t i = 0;
         for(auto port : commonPorts) {
@@ -203,9 +356,8 @@ ISR(TIMER2_COMPA_vect)        // uint16_terrupt service routine
     }
 
     {
-        uint8_t i = 0;
         for(auto port : segmentPorts) {
-            if(i == segmentTestPhase && common == commonTestPhase) {
+            if((segmentPinsToLight & 0b1)) {
                 if(commonUpPhase) {
                     cbi(buildPorts[port.portNumber], port.offset);
                 } else {
@@ -218,7 +370,7 @@ ISR(TIMER2_COMPA_vect)        // uint16_terrupt service routine
                     cbi(buildPorts[port.portNumber], port.offset);
                 }
             }
-            ++i;
+            segmentPinsToLight >>= 1;
         }
     }
         
@@ -231,30 +383,93 @@ ISR(TIMER2_COMPA_vect)        // uint16_terrupt service routine
         }
     }
     
+    phase = phase + 1;
+    if(phase == 8) {
+        phase = 0;
+
+        // Copy this once per cycle, instead of reading it directly,
+        // so that eacy entire cyce has a 'balanced' AC voltage for
+        // each segment.
+        // Not sure if this is actually necessary?
+        // Do it at the end of the interrupt so that the above stuff
+        // runs at 'more constant' time w.r.t. the timer firing.
+        memcpy(segmentPinStateByCommon, displaySegmentStateByCommon, sizeof(displaySegmentStateByCommon));
+    }
+
     interrupts();
-   
-    ++counter;
-    
-    static const uint8_t resetSteps = []() {
-        uint8_t i = (LCD_AC_FREQ * 2);
-        i = (i + 7) / 8 * 8;
-        return i;
-    }();
-    
-    if((counter % resetSteps) == 0) {
-        counter = 0;
-        if(++segmentTestPhase == 12) {
-            segmentTestPhase = 0;
-            if(++commonTestPhase == 4) {
-                commonTestPhase = 0;
+}
+
+void SetDigits(uint16_t digits) {
+    uint16_t segmentPinsToLight[4] = {0};
+
+    if(digits >= 1000) {
+        digits = 999;
+    }
+
+    {
+        auto digit = digits % 10;
+        uint8_t segmentBitPattern = SevenSegmentCodes[digit];
+        for(int i = 0; segmentBitPattern != 0; ++i) {
+            if(segmentBitPattern & 0b1) {
+                LEDSymbol symbol = DigitSegmentSymbols[0][i];
+                segmentPinsToLight[symbol.CommonPin()] |= _BV(symbol.SegmentPin());
             }
+            segmentBitPattern >>= 1;
         }
     }
+
+    if(digits >= 10) {
+        auto digit = (digits / 10) % 10;
+        uint8_t segmentBitPattern = SevenSegmentCodes[digit];
+        for(int i = 0; segmentBitPattern != 0; ++i) {
+            if(segmentBitPattern & 0b1) {
+                LEDSymbol symbol = DigitSegmentSymbols[1][i];
+                segmentPinsToLight[symbol.CommonPin()] |= _BV(symbol.SegmentPin());
+            }
+            segmentBitPattern >>= 1;
+        }
+    }
+
+    if(digits >= 100) {
+        auto digit = (digits / 100) % 10;
+        uint8_t segmentBitPattern = SevenSegmentCodes[digit];
+        for(int i = 0; segmentBitPattern != 0; ++i) {
+            if(segmentBitPattern & 0b1) {
+                LEDSymbol symbol = DigitSegmentSymbols[2][i];
+                segmentPinsToLight[symbol.CommonPin()] |= _BV(symbol.SegmentPin());
+            }
+            segmentBitPattern >>= 1;
+        }
+    }
+
+    noInterrupts();
+
+    for(int i = 0; i < 4; ++i) {
+        displaySegmentStateByCommon[i] = (displaySegmentStateByCommon[i] & ~(DigitSegmentPortMaskForCommon[i])) | segmentPinsToLight[i];
+    }
+
+    interrupts();
 }
 
 void loop()
-{
-    
-	    
+{    
+    auto timeNow = millis();
+
+    static typeof(timeNow) halfSeconds = 0;
+    typeof(timeNow) halfSecondsNow = (timeNow / 500);
+    if(halfSecondsNow != halfSeconds) {
+        halfSeconds = halfSecondsNow;
+        if(halfSecondsNow % 2) {
+            SetDigits(halfSecondsNow / 2);
+        }
+
+        LEDSymbol symbol = LEDSymbol::CautionTriangle;
+        uint16_t bit = _BV(symbol.SegmentPin());
+        if((displaySegmentStateByCommon[symbol.CommonPin()] & bit) == bit) { 
+            displaySegmentStateByCommon[symbol.CommonPin()] &= (~bit);
+        } else {
+            displaySegmentStateByCommon[symbol.CommonPin()] |= bit;
+        }
+    }
 }
 
