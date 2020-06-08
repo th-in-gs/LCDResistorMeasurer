e3afa766ac036803aa85801814a36aad5dd789e9
James Montgomerie <jamie@montgomerie.net>
2020-06-07 21:06:40 -0700

    Various improvements.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h> 
 #include <util/atomic.h>
-
+ 
 #include "LCDDisplay.h"
 #include "ADCTables.h"
 #include "Conversions.h"
@@ -13,19 +13,19 @@ static const auto ADCReadingBufferLength = sizeof(ADCReadings) / sizeof(ADCReadi
 static volatile uint16_t ADCReadCount = 0;
 
 void setup()
-{
+{        
+    // Set up AVCC with external capacitor at AREF pin, ADC 5 selected. 
+  	ADMUX = 0b01000101;
+
     display.SetUp();  
 
     // Bits 0-4 of PORTC are used to select test resistor.
     // Leave as high-impedance, external pull-ups will pull PNP bases to V+
 
     // Bit 5 is used for ADC input. Leave it as input high-impedance
-        
+
     // Set up ADC options. Slowest conversion, interrupts.
     ADCSRA = (1<<ADEN) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
-    
-    // AVCC with external capacitor at AREF pin, ADC 5 selected. 
-  	ADMUX = 0b01000101;
 
     // Start Conversion
     ADCSRA |= 1<<ADSC;		
@@ -58,10 +58,6 @@ static const auto ReferenceResistorValuesLength = sizeof(ReferenceResistorValues
     
 void loop()
 { 
-    // Rotate through values to find one in 233 - 787 range.
-    // If we're at the first table, use 0 - 787 range.
-    // If we're at the last table, use 233 - 787 range. 
-
     static unsigned long lineHeldLowTime = 0;
 
     static uint8_t referenceResistorValueIndex = ReferenceResistorValuesLength - 1;
@@ -69,7 +65,25 @@ void loop()
 
     static uint8_t eValuesIndex = 0;
 
+    // Rotate through values to find one in 233 - 787 range.
+    // If we're at the first table, use 0 - 787 range.
+    // If we're at the last table, use 233 - 787 range. 
     if(nextReferenceResistorValueIndex != referenceResistorValueIndex) {
+        /*
+        // Debug code - show which table is being used on the top row of the 
+        // LCD.
+        static const LCDDisplay::LEDSymbol symbols[] = {
+            LCDDisplay::LEDSymbol::One,
+            LCDDisplay::LEDSymbol::Radiator,
+            LCDDisplay::LEDSymbol::Two,
+            LCDDisplay::LEDSymbol::Tap,
+            LCDDisplay::LEDSymbol::TriangleInCircle,
+        };
+        
+        display.ClearSymbol(symbols[referenceResistorValueIndex]);
+        display.SetSymbol(symbols[nextReferenceResistorValueIndex]);
+        */
+
         // Old port back to input, high impedence.
         cbi(DDRC, referenceResistorValueIndex);
         
@@ -114,6 +128,7 @@ void loop()
         } 
     } while(!ADCValueValid);
     
+    
     // Check if we're in the 'good' range (with good resolution),
     // try another table next time if not.
     if(sampledADCValue < 233 && referenceResistorValueIndex != 0) {
@@ -122,7 +137,7 @@ void loop()
         nextReferenceResistorValueIndex = referenceResistorValueIndex + 1;
     }
                 
-                
+                    
     // A switch that closes the test resistor contacts serves as an input 
     // If it's held for over 1 second, switch the E series we're rounding to.
     bool shouldDisplaySwitchedEValues = false;
@@ -134,7 +149,7 @@ void loop()
         
         shouldDisplaySwitchedEValues = true;
         
-        if((nowTime - lineHeldLowTime) > 1000) {
+        if((nowTime - lineHeldLowTime) > 750) {
             eValuesIndex = (eValuesIndex + 1) % 3;
             lineHeldLowTime = 0;
         }
@@ -142,35 +157,50 @@ void loop()
         lineHeldLowTime = 0;
     }
     
+    
+    static uint8_t lastDisplayedEValuesIndex = 2;
+    if(eValuesIndex != lastDisplayedEValuesIndex) {
+        if(lastDisplayedEValuesIndex < 2) {
+            display.ClearSymbol(lastDisplayedEValuesIndex == 0 ? 
+                                LCDDisplay::LEDSymbol::DownArrowOnLeft : 
+                                LCDDisplay::LEDSymbol::UpArrowOnLeft);
+        }
+        if(eValuesIndex < 2) {
+            display.SetSymbol(eValuesIndex == 0 ? 
+                              LCDDisplay::LEDSymbol::DownArrowOnLeft : 
+                              LCDDisplay::LEDSymbol::UpArrowOnLeft);
+        }
+        lastDisplayedEValuesIndex = eValuesIndex;
+    } 
+
+
     if(shouldDisplaySwitchedEValues) {      
         display.ClearSymbol(LCDDisplay::LEDSymbol::Colon);
         display.ClearSymbol(LCDDisplay::LEDSymbol::DecimalPoint);
         display.ClearSymbol(LCDDisplay::LEDSymbol::DownArrowOnRight);
         display.ClearSymbol(LCDDisplay::LEDSymbol::UpArrowOnRight);
-        display.ClearSymbol(LCDDisplay::LEDSymbol::DottedUpsidedownT);
-        display.ClearSymbol(LCDDisplay::LEDSymbol::ChartGoingNegative);
+        display.ClearSymbol(LCDDisplay::LEDSymbol::MinusLeft);
+        display.ClearSymbol(LCDDisplay::LEDSymbol::Plus);
+        display.ClearSymbol(LCDDisplay::LEDSymbol::Target);
 
         display.SetString(((const char *[]){"E24", "E96", "---"})[eValuesIndex]);
     } else {
         uint32_t resistorValue = ADCToOhms(sampledADCValue, ReferenceResistorValues[referenceResistorValueIndex]);    
+        //uint32_t resistorValue = sampledADCValue;
         
         if(resistorValue >= 1000000000 ||
            sampledADCValue >= 950 ||
-           resistorValue <= 5) {
-            display.SetDigits(-1);
-            
+           resistorValue <= 5) {            
             display.ClearSymbol(LCDDisplay::LEDSymbol::Colon);
             display.ClearSymbol(LCDDisplay::LEDSymbol::DecimalPoint);
             display.ClearSymbol(LCDDisplay::LEDSymbol::DownArrowOnRight);
             display.ClearSymbol(LCDDisplay::LEDSymbol::UpArrowOnRight);
-
-            display.SetSymbol(LCDDisplay::LEDSymbol::DottedUpsidedownT);
-            display.SetSymbol(LCDDisplay::LEDSymbol::ChartGoingNegative);
-        } else {   
-            display.ClearSymbol(LCDDisplay::LEDSymbol::DottedUpsidedownT);
-            display.ClearSymbol(LCDDisplay::LEDSymbol::ChartGoingNegative);
+            display.ClearSymbol(LCDDisplay::LEDSymbol::MinusLeft);
+            display.ClearSymbol(LCDDisplay::LEDSymbol::Plus);
+            display.ClearSymbol(LCDDisplay::LEDSymbol::Target);
             
-
+            display.SetDigits(-1);
+        } else {   
             uint32_t roundedResistorValue = resistorValue;
 
             static const uint16_t *valuesTable;
@@ -212,43 +242,43 @@ void loop()
                 // Use the C bsearch routine (a little unorthodoxly) to find the
                 // nearest table value.
                 const size_t nearestElementIndex = (size_t)bsearch(
-                &leading4Digits,
-                0, // This takes a pointer really, but we're coercing it to behave like it takes indexes.
-                valuesTableCount,
-                1, // Size is also 1 to coerce index-like behaviour.
-                [](const void *key, const void *element) {
-                    const uint16_t resistorValue = *((uint16_t *)key);
-                    const size_t index = (uint8_t)(size_t)element;
-                    
-                    // Treat each entry in the ADC table as a 'bucket' stretching
-                    // between the two adjacent values.
-                    
-                    const uint16_t prevValue = [index]() { 
-                        if(index == 0) {
-                            return (uint16_t)0;
+                    &leading4Digits,
+                    0, // This takes a pointer really, but we're coercing it to behave like it takes indexes.
+                    valuesTableCount,
+                    1, // Size is also 1 to coerce index-like behaviour.
+                    [](const void *key, const void *element) {
+                        const uint16_t resistorValue = *((uint16_t *)key);
+                        const size_t index = (uint8_t)(size_t)element;
+                        
+                        // Treat each entry in the ADC table as a 'bucket' stretching
+                        // between the two adjacent values.
+                        
+                        const uint16_t prevValue = [index]() { 
+                            if(index == 0) {
+                                return (uint16_t)0;
+                            } else {
+                                return pgm_read_word_near(valuesTable + index - 1); 
+                            }
+                        }();
+                        const uint16_t value = pgm_read_word_near(valuesTable + index);
+                        const uint16_t nextValue = [index]() { 
+                            if(index + 1 < valuesTableCount) {
+                                return pgm_read_word_near(valuesTable + index + 1);
+                            } else {
+                                return UINT16_MAX; 
+                            }
+                        }();
+                        
+                        if(resistorValue == value) {
+                            return 0;
+                        } else if(resistorValue <= (value - (value - prevValue) / 2)) {
+                            return -1;
+                        } else if(resistorValue > (nextValue - (nextValue - value) / 2)) {
+                            return 1;
                         } else {
-                            return pgm_read_word_near(valuesTable + index - 1); 
+                            return 0;
                         }
-                    }();
-                    const uint16_t value = pgm_read_word_near(valuesTable + index);
-                    const uint16_t nextValue = [index]() { 
-                        if(index + 1 < valuesTableCount) {
-                            return pgm_read_word_near(valuesTable + index + 1);
-                        } else {
-                            return UINT16_MAX; 
-                        }
-                    }();
-                    
-                    if(resistorValue == value) {
-                        return 0;
-                    } else if(resistorValue <= (value - (value - prevValue) / 2)) {
-                        return -1;
-                    } else if(resistorValue > (nextValue - (nextValue - value) / 2)) {
-                        return 1;
-                    } else {
-                        return 0;
                     }
-                }
                 );
                 
                 roundedResistorValue = (uint32_t)pgm_read_word_near(valuesTable + nearestElementIndex);
@@ -341,9 +371,9 @@ void loop()
                     break;
                 }
                 default: {
-                    // Error
-                    display.SetSymbol(LCDDisplay::LEDSymbol::Colon);
-                    display.SetSymbol(LCDDisplay::LEDSymbol::DecimalPoint);
+                    // Error.
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::Colon);
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::DecimalPoint);
                     display.SetSymbol(LCDDisplay::LEDSymbol::CautionTriangle);
                     break;
                 }
@@ -367,12 +397,44 @@ void loop()
                 }
                 default: {
                     // Error.
-                    display.SetSymbol(LCDDisplay::LEDSymbol::DownArrowOnRight);
-                    display.SetSymbol(LCDDisplay::LEDSymbol::UpArrowOnRight);
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::DownArrowOnRight);
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::UpArrowOnRight);
                     display.SetSymbol(LCDDisplay::LEDSymbol::TriangleInCircle);
                     break;
                 }
-            }        
+            }
+            
+            if(resistorValue != roundedResistorValue) {
+                uint32_t difference;
+                if(resistorValue > roundedResistorValue) {
+                    difference = resistorValue - roundedResistorValue;
+                } else {
+                    difference = roundedResistorValue - resistorValue;
+                }
+                
+                // Are we within 5%, for E24, or 1%, for E96?
+                uint8_t percentageTimes10 = ((difference * 1000) / roundedResistorValue);
+                if(eValuesIndex != 2 &&
+                    (percentageTimes10 <= 50 && 
+                    (percentageTimes10 <= 10 || eValuesIndex == 0))) {
+                    display.SetSymbol(LCDDisplay::LEDSymbol::Target);
+                } else {
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::Target);
+                }
+                
+                // Display if the measured value is higher or lower than the
+                // E value.
+                if(resistorValue > roundedResistorValue) {
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::MinusLeft);
+                    display.SetSymbol(LCDDisplay::LEDSymbol::Plus);
+                } else if(resistorValue < roundedResistorValue) {
+                    display.SetSymbol(LCDDisplay::LEDSymbol::MinusLeft);
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::Plus);
+                } else {
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::MinusLeft);
+                    display.ClearSymbol(LCDDisplay::LEDSymbol::Plus);
+                }
+            }
         }
     }
 }
